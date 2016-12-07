651ed7ee36e0216e4ba99978f2acf8e3049a5064
kristoferno <kristofer.nordin@gmail.com>
2016-12-07 22:01:07 +0100

    added localstorage support
    added local storage to remember opened items 
    removed the ghastly datecheater-dropdown

--- assets/js/my_jquery_functions.js
+++ assets/js/my_jquery_functions.js
@@ -1,87 +1,86 @@
 $(document).ready(function() {
+    var daysToXmas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    //24 calendar items, this will be randomized later
-    var daysToXmas = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'];
+    /*
+    localStorage.setItem('daysToXmas', JSON.stringify(daysToXmas));
+
+    daysToXmas = JSON.parse(localStorage.getItem('daysToXmas'));
+    */
+    if (localStorage.getItem('daysToXmas') === null) {
+        localStorage.setItem('daysToXmas', daysToXmas);
+    } else {
+        var myStoredArray = localStorage.getItem('daysToXmas').split(',');
+        for (i = 0; i < 24; i++) {
+            daysToXmas[i] = parseInt(myStoredArray[i]);
+        }
+    }
 
     //Here we get which day of the month it is today
     var today = new Date();
     var dd = today.getDate();
 
-    //init the date cheat form with todays date.
-    $("#setDate").val(dd);
+    for (i = 0; i < 24; i++) {
 
+        currDay = i + 1;
 
-    $.each(daysToXmas, function(index, value) {
+        if (daysToXmas[i] === 0) {
+            $(".calendar-panel").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-2"><div class="panel panel-default"><div id="' + currDay + '" class="panel-body image"><img id="luckbild-' + currDay + '" class="img-responsive img-thumbnail" src="assets/image/' + currDay + '.jpg"><h2 id="lucka-' + currDay + '">' + currDay + '</h2></div></div></div>');
 
-        /*
-        The following code snippet to pick out random array value. 
-        Borrowed from stackoverflow:
-        http://stackoverflow.com/questions/4550505/getting-random-value-from-an-array
-        */
-        var randomDay = daysToXmas[Math.floor(Math.random() * daysToXmas.length)];
+        } else {
+            $(".calendar-panel").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-2"><div class="panel panel-default"><div id="' + currDay + '" class="panel-body image"><img id="luckbild-' + currDay + '" class="img-responsive img-thumbnail" src="assets/image/' + currDay + '.jpg"><h2 id="lucka-' + currDay + '"class="hidden">' + currDay + '</h2></div></div></div>');
 
-        daysToXmas = jQuery.grep(daysToXmas, function(value) {
-            return value != randomDay;
-        });
+        }
+    }
 
-        /*
-        Append a advent calendar item under the "calendar-panel" element in DOM
-        */
-        $(".calendar-panel").append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-2"><div class="panel panel-default"><div id="' + randomDay + '" class="panel-body image"><img id="luckbild-' + randomDay + '" class="img-responsive img-thumbnail" src="assets/image/' + randomDay + '.jpg"><h2 id="lucka-' + randomDay + '">' + randomDay + '</h2></div></div></div>');
-        /*
-		The above will append the following block to the DOM tree (assuming "randomDay=1")
-		
-		<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2 col-xl-2">
-        	<div class="panel panel-default">
-          		<div id="1" class="panel-body image">
-            		<img id="luckbild-1" class="img-responsive img-thumbnail" src="assets/image/1.jpg">
-            		<h2 id="lucka-1">1</h2>    
-          		</div>
-        	</div>
-      	</div>
+    for (i = 0; i < 24; i++) {
 
-		*/
+        currDay = i + 1;
 
+        if (daysToXmas[i] === 1) {
 
-        //Stop doing this when we have added 24 items
-        return (value !== '24');
-    });
+            $("#luckbild-" + currDay).css({ opacity: 1 });
+            $("#luckbild-" + currDay).addClass('opened');
+            $("#lucka-" + currDay).addClass('hidden');
+        }
+    }
 
 
     /*
-    This is where we open an advent item.
-    When opening, we set the IMG opacity to 1 
+    This is where we 0 an advent item.
+    When 0ing, we set the IMG opacity to 1 
     and then adds class hidden to the h2 containing the advent number
 
-    An advent item will only open if its ID is less than or equal todays date.
+    An advent item will only 0 if its ID is less than or equal todays date.
     */
     $(".image").bind('click', function() {
 
         var myID = $(this).attr('id');
-
-        var dateFromInput = parseInt($("#setDate").val());
+        var myArrayIndex = myID - 1;
 
         /*
         Check so we are allowed to reveal the clicked item
         */
-        if (myID <= dateFromInput) {
+        if (myID <= dd) {
             /*
-            Check if the clicked item is already opened. 
+            Check if the clicked item is already 0ed. 
             In that case, we close it.
             */
             if ($("#luckbild-" + myID).hasClass('opened')) {
                 $("#luckbild-" + myID).css({ opacity: 0 });
                 $("#luckbild-" + myID).removeClass('opened');
                 $("#lucka-" + myID).removeClass('hidden');
+                daysToXmas[myArrayIndex] = 0;
             }
             /*
-            If the clicked item is not already open, reveal it now
+            If the clicked item is not already 0, reveal it now
             */
             else {
                 $("#luckbild-" + myID).css({ opacity: 1 });
                 $("#luckbild-" + myID).addClass('opened');
                 $("#lucka-" + myID).addClass('hidden');
+                daysToXmas[myArrayIndex] = 1;
             }
+            localStorage.setItem('daysToXmas', daysToXmas);
         }
         /*
         If we are not allowed to reveal clicked item, 
